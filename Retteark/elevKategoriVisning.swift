1b78b5e4e6d9a59ff04b5b876e22eccfa361b2f3
Stein Angel Braseth <steinang@gmail.com>
2022-09-05 21:58:01 +0200

    La til visning av elevtilbakemleding.

--- Retteark/elevKategoriVisning.swift
+++ Retteark/elevKategoriVisning.swift
@@ -13,16 +13,39 @@ struct elevTilbakemeldingVisning: View {
     @ObservedObject var prøve: Prøve
     
     var body: some View {
-        HStack{
-            VStack {
+        VStack{
+            HStack {
                 Button("Tilbake") {
                     visElevTilbakemleding = nil
                 }
-                .frame(alignment: .leading)
             }
             Text(elev.navn)
+            ForEach(0..<prøve.kategorier.count) { kategoriIndex in
+                /*kakediagram(desimaltall: Double(elevPoengKategori(elevIndex: prøve.elever.firstIndex{$0.id == elev.id}!, kategoriIndex: kategoriIndex)/maxPoengKategori(kategoriIndex: kategoriIndex)), farge:Color.red).frame(width: 200, height: 200, alignment: .center)*/
+                Text(String(Double(elevPoengKategori(elevIndex: prøve.elever.firstIndex{$0.id == elev.id}!, kategoriIndex: kategoriIndex)/maxPoengKategori(kategoriIndex: kategoriIndex))))
+            }
         }
             
         }
+    
+    func maxPoengKategori(kategoriIndex: Int) -> Float {
+        var sum: Float = 0
+        for oppgaveIndex in 0..<prøve.kategorierOgOppgaver[kategoriIndex].count {
+            if(prøve.kategorierOgOppgaver[kategoriIndex][oppgaveIndex]){
+                sum += prøve.oppgaver[oppgaveIndex].maksPoeng ?? 0
+            }
+        }
+        return sum
+    }
+    
+    func elevPoengKategori(elevIndex: Int, kategoriIndex: Int) -> Float {
+        var sum: Float = 0
+        for oppgaveIndex in 0..<prøve.kategorierOgOppgaver[kategoriIndex].count {
+            if(prøve.kategorierOgOppgaver[kategoriIndex][oppgaveIndex]){
+                sum += prøve.poeng[elevIndex][oppgaveIndex].poeng ?? 0
+            }
+        }
+        return sum
+    }
         
     }
