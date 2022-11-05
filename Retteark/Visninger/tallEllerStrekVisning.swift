5317c441a17b395ad22f23726112741a4ae3c1bc
kappekoff <47227587+kappekoff@users.noreply.github.com>
2022-11-05 11:48:49 +0100

    lagt til tallEllerStrekVisning

---
+++ Retteark/Visninger/tallEllerStrekVisning.swift
@@ -0,0 +1,82 @@
+//
+//  tallEllerStrekVisning.swift
+//  Retteark
+//
+//  Created by Stein Angel Braseth on 05/11/2022.
+//
+
+import SwiftUI
+import RegexBuilder
+
+struct tallEllerStrekVisning: View {
+    @Binding var tekst: String
+    @State var tittel: String
+    
+    var body: some View {
+        TextField(tittel, text: $tekst)
+            .border(.black)
+            .onChange(of: tekst, perform: endretTall(nyVerdi:))
+
+        
+    }
+    func endretTall(nyVerdi: String) {
+        let tallSymbol = Regex {
+            ChoiceOf {
+                "0"
+                "1"
+                "2"
+                "3"
+                "4"
+                "5"
+                "6"
+                "7"
+                "8"
+                "9"
+            }
+        }
+        let punkt = Regex {
+            ChoiceOf {
+                "."
+                ","
+            }
+        }
+        let tall = Regex {
+            OneOrMore(tallSymbol)
+            Optionally {
+                punkt
+                Optionally {
+                    OneOrMore(tallSymbol)
+                }
+                
+            }
+        }
+        let strek = Regex {
+            ChoiceOf {
+                "-"
+                "–"
+                "—"
+            }
+        }
+        let tallEllerStrek = Regex {
+            ChoiceOf {
+                ""
+                tall
+                strek
+            }
+        }
+        
+        if let _ = nyVerdi.wholeMatch(of: tallEllerStrek) {
+            tekst = nyVerdi
+            tittel = tekst
+        }
+        else {
+            tekst = tittel
+        }
+        
+        let formatter = NumberFormatter()
+        
+        
+    }
+}
+
+
