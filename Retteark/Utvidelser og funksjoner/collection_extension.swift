abaf2eeb36b2a863476a75d0164a3332673b97b8
kappekoff <47227587+kappekoff@users.noreply.github.com>
2022-11-28 10:40:15 +0100

    Oppdatert SkrivInnOppgaver

---
+++ Retteark/Utvidelser og funksjoner/collection_extension.swift
@@ -0,0 +1,8 @@
+//
+//  collection_extension.swift
+//  Retteark
+//
+//  Created by Stein Angel Braseth on 24/11/2022.
+//
+
+import Foundation
---
+++ Retteark/Utvidelser og funksjoner/skrivInnOppgaver.swift
@@ -0,0 +1,8 @@
+//
+//  skrivInnOppgaver.swift
+//  Retteark
+//
+//  Created by Stein Angel Braseth on 23/11/2022.
+//
+
+import Foundation
